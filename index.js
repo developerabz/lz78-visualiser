9e8c50c30f9013253ddd4c32a0bf0ed2064adb74
Abdullah Yakub <abdullah.yakub@hotmail.com>
2023-11-25 18:06:00 +1100

    fix lz78 algorithm, add more statistcs to output html, refactor js

--- index.js
+++ index.js
@@ -1,91 +1,117 @@
-const root = document.getElementById('root')
-
-const letters = [...root.textContent]
 // console.log(letters)
 const body = document.querySelector('body')
-const visual = [{
-  index: 0,
-  output: `(${0}, '')`,
-  code: ''
-}]
 
-let i = 0
-while (i < letters.length) {
-  let matched = false
-  for (const v of visual) {
-    // console.log("V code: " + v.code)
-    // console.log(letters[i])
-    if (v.code.includes(letters[i])) {
-      matched = true
-      let string = letters[i]
+const button = document.querySelector('button');
+
+
+const removeElement = (identifier) => {
+  if (body.querySelector(identifier)) {
+    body.removeChild(body.querySelector(identifier))
+  }
+}
+
+const removeExistingElements = () => {
+  removeElement('p');
+  removeElement('.list')
+}
+
+const runLz78Algo = (letters, visual) => {
+  let i = 0
+  while (i < letters.length) {
+    const matches = visual.filter(v => v.code[0] && v.code[0] === letters[i])
+
+    if (matches.length === 0) {
+      visual.push({
+        index: visual.length,
+        output: `(${0}, '${letters[i]}')`,
+        code: `${letters[i]}`
+      })
       i++
-      while (i < letters.length && v.code.includes(letters[i])) {
-        string += letters[i]
-        i++
-      }
-      if (i < letters.length) {
-        string += letters[i]
-        // console.log("added here")
-        visual.push({
-          index: visual.length,
-          output: `(${v.index}, '${letters[i]}')`,
-          code: `'${string}'`
+    } else {
+      matches.reverse()
+      for (const m of matches) {
+        const completeMatch = [...m.code].every((char, index) => {
+          return char === letters[i + index]
         })
-        break
+        if (completeMatch) {
+          const outputStr = letters[i + m.code.length] || 'Text End'
+          visual.push({
+            index: visual.length,
+            output: `(${m.index}, ${outputStr})`,
+            code: `${m.code + outputStr}`
+          })
+          i += m.code.length + 1;
+          break
+        }
       }
     }
-    // console.log(matched)
+  }
+}
 
-  }
-  if (!matched && i < letters.length) {
-    // console.log("nope here")
-    visual.push({
-      index: visual.length,
-      output: `(${0}, '${letters[i]}')`,
-      code: `'${letters[i]}'`
-    })
-    i++
-  }
+const calcReduction = (orginalAmount, newAmount) => {
+  const reducedAmount = (orginalAmount - newAmount)/orginalAmount * 100
+  return reducedAmount.toFixed(2)
+}
+
+const addInfoToBody = (root, visual) => {
+  const p = document.createElement('p');
+  p.textContent = `The original text required ${root.value.length} characters. After compression it requires only ${visual.length} characters. A reduction of approximately ${calcReduction(root.value.length, visual.length)}%.`
+  body.append(p)
+  // console.log(visual)
+  const list = document.createElement('div');
+  list.classList.add('list')
+  const cardTitle = document.createElement('div')
+  cardTitle.classList.add('card')
+  const indexTitle = document.createElement('span')
+  indexTitle.textContent = 'Index'
+  indexTitle.classList.add('col')
+  const outputTitle = document.createElement('span')
+  outputTitle.textContent = 'Output'
+  outputTitle.classList.add('col')
+  
+  const codeTitle = document.createElement('span')
+  codeTitle.textContent = 'Code'
+  codeTitle.classList.add('col')
+  
+  cardTitle.append(indexTitle, outputTitle, codeTitle)
+  list.append(cardTitle)
+  visual.forEach(v => {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    const index = document.createElement('span')
+    index.textContent = v.index
+    index.classList.add('col')
+  
+    const output = document.createElement('span')
+    output.textContent = v.output
+    output.classList.add('col')
+  
+    const code = document.createElement('span')
+    code.textContent = `'${v.code}'`
+    code.classList.add('col')
+  
+    card.append(index, output, code)
+    list.append(card)
+  })
+  body.append(list)
+}
+
+
+const produceVisualisation = () => {
+  removeExistingElements();
+
+  const root = document.getElementById('root')
+  const letters = [...root.value]
+
+  const visual = [{
+    index: 0,
+    output: `(${0}, '')`,
+    code: ''
+  }]
+  runLz78Algo(letters, visual);
+
+  addInfoToBody(root, visual)
 
 }
-const p = document.createElement('p');
-p.textContent = `The original text required ${root.textContent.length} characters. After compression it requires only ${visual.length} characters.`
-body.append(p)
-// console.log(visual)
-const list = document.createElement('div');
-list.classList.add('list')
-const cardTitle = document.createElement('div')
-cardTitle.classList.add('card')
-const indexTitle = document.createElement('span')
-indexTitle.textContent = 'Index'
-indexTitle.classList.add('col')
-const outputTitle = document.createElement('span')
-outputTitle.textContent = 'Output'
-outputTitle.classList.add('col')
 
-const codeTitle = document.createElement('span')
-codeTitle.textContent = 'Code'
-codeTitle.classList.add('col')
-
-cardTitle.append(indexTitle, outputTitle, codeTitle)
-list.append(cardTitle)
-visual.forEach(v => {
-  const card = document.createElement('div')
-  card.classList.add('card')
-  const index = document.createElement('span')
-  index.textContent = v.index
-  index.classList.add('col')
-
-  const output = document.createElement('span')
-  output.textContent = v.output
-  output.classList.add('col')
-
-  const code = document.createElement('span')
-  code.textContent = v.code
-  code.classList.add('col')
-
-  card.append(index, output, code)
-  list.append(card)
-})
-body.append(list)
-
+button.addEventListener('click', produceVisualisation)
